db7cf0963d415c1125460e6ecaf0f0da40088545
Nathen <13147177623@163.com>
2020-08-20 13:51:13 +0800

    add builder -- ts

---
+++ np-cli-ts/src/index/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+function APP(){
+
+  return (
+    <div>
+      welcome to!!!
+    </div>
+  )
+}
+
+reactDOM.render(<APP />,document.getElementById('root'));
---
+++ np-cli-ts/webpack.dev.config.js
@@ -0,0 +1,56 @@
+const merge= require('webpack-merge');
+const baseConfig = require('./webpack.base.config');
+
+const devConfig = {
+  mode: 'development',
+  watch:true,
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000, 
+    aggregateTimeout: 300 
+  },
+  module:{
+    rules: [
+      {
+        test: /\.(le|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '_[name]_[local]-[hash:base64:5]'
+              },
+            }
+          },
+          'postcss-loader',
+          'less-loader'
+        ],
+        exclude:/node_modules/
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        baseReact: {
+          test: /(react|react-dom)/,
+          name: 'baseReact',
+          chunks: 'all',
+          priority: 0,
+        },
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'all',
+          priority: -1,
+        }
+      }
+    },
+    runtimeChunk: {   
+        name: "manifest"
+      }
+  },
+  // devtool:'source-map'
+}
+module.exports = merge(baseConfig,devConfig);
